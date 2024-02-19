e0d878e2f41cd7d80af837d6ab1b030bbca8f404
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-19 17:14:00 +0200

    .

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const dotenv = require('dotenv').config()
 
 const mongoose = require ('mongoose')
 DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/"
-mongoose.connect((DATABASE_URL), { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.once('open',()=>{console.log('connected to mongo DB')})
-db.on('error', error=>{console.log(error)})
-
+mongoose.connect(DATABASE_URL)
+  .then((result) => {
+    console.log('Connected to the DataBase successfully');
+  })
+  .catch((err) => console.log(err));
 
 
 app.get('/', (req, res) => {
