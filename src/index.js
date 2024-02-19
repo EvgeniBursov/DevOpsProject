1ff14ecac467914fd528b15678276332f6caf6bf
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-19 15:41:06 +0200

    add URL

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const dotenv = require('dotenv').config()
 
 
 const mongoose = require ('mongoose')
-mongoose.connect(process.env.DATABASE_URL)//,{ useNewUrlParser: true, useUnifiedTopology: true });
-console.log(process.env.DATABASE_URL)
+DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/"
+mongoose.connect(DATABASE_URL)
 const db = mongoose.connection
 db.once('open',()=>{console.log('connected to mongo DB')})
 db.on('error', error=>{console.log(error)})
