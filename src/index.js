04869e481f4bd040110f60e8d365e4ce2bcaa7ef
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-19 15:04:36 +0200

    change env file

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const dotenv = require('dotenv').config()
 
 
 const mongoose = require ('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL)//,{ useNewUrlParser: true, useUnifiedTopology: true });
+console.log(process.env.DATABASE_URL)
 const db = mongoose.connection
-db.on('error', error=>{console.log(error)})
 db.once('open',()=>{console.log('connected to mongo DB')})
+db.on('error', error=>{console.log(error)})
+
 
 
 app.get('/', (req, res) => {
