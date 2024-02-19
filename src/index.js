d63ac16c0a689655b77905819e2adee42f3e8aec
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-19 21:04:23 +0200

    add html

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 const dotenv = require('dotenv').config()
 
+app.use(express.static('public'))
 
 const mongoose = require ('mongoose')
 DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/"
@@ -15,6 +16,11 @@ mongoose.connect(DATABASE_URL)
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi from server main page<h2>');
+});
+
+app.get('/register', (req, res) => {
+  // Use path.join to ensure correct file path across different operating systems
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
 app.listen(port, () => {
