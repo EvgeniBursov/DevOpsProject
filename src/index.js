735018002232b8a743c6c1956deffc66021e9faa
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-19 14:50:53 +0200

    connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
+const dotenv = require('dotenv').config()
+
+
+const mongoose = require ('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error=>{console.log(error)})
+db.once('open',()=>{console.log('connected to mongo DB')})
+
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi from server main page<h2>');
