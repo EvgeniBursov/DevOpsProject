e5e94fefe45dfcca6ad79fbdd480555066e4e4d9
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-02-27 22:51:17 +0200

    little changes in login and register page

--- public/js/nav.js
+++ public/js/nav.js
@@ -9,7 +9,13 @@ const createNav = () => {
                     <input type="text" class="search-box" placeholder="search brand, product">
                     <button class="search-btn">search</button>
                 </div>
-                <a href="#"><img src="img/user.png" alt=""></a>
+                <a>
+                <img src="img/user.png" id="user-img" alt="">
+                <div class="login-logout-popup hide">
+                    <p class="account-info">Log in as, name</p>
+                    <button class="btn" id="user-btn">Log out</button>
+                </div>
+                </a>
                 <a href="#"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
@@ -24,3 +30,12 @@ const createNav = () => {
 }
 
 createNav();
+
+const userImageButton = document.querySelector('#user-img');
+const userPop = document.querySelector('.login-logout-popup');
+const popuptext = document.querySelector('.account-info');
+const actionBtn = document.querySelector('#user-btn');
+
+userImageButton.addEventListener('click', () => {
+    userPop.classList.toggle('hide')
+})
