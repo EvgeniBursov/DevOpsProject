b3ccc2b0ef8b7bd89cf1b42bcc2fdd30c75a335a
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-03-05 10:56:27 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-const express = require('express');
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
 const port = process.env.PORT || 4000;
 const app = express();
-const dotenv = require('dotenv').config()
+const result = dotenv.config()
+
+const DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/"
+mongoose.connect(DATABASE_URL)
+.then((result) => {
+  console.log('Connected to the DataBase successfully');
+})
+.catch((err) => console.log(err));
 
 app.use(express.static('public'))
-
-const mongoose = require ('mongoose')
-DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/"
-mongoose.connect(DATABASE_URL)
-  .then((result) => {
-    console.log('Connected to the DataBase successfully');
-  })
-  .catch((err) => console.log(err));
+app.use(cors())
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hi from server main page<h2>');
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/register', (req, res) => {
