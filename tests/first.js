8436373d2a1471ebc609cfa852dd32a8a0460018
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-03-06 14:48:00 +0200

    add selenium

---
+++ tests/first.js
@@ -0,0 +1,32 @@
+import { By, Builder } from 'selenium-webdriver';
+import assert from 'assert';
+
+async function firstTest() {
+  let driver;
+  
+  try {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('https://devopsproject-v74y.onrender.com/');
+  
+    let title = await driver.getTitle();
+    assert.equal("Web Store", title);
+  
+    await driver.manage().setTimeouts({ implicit: 10000 });
+  
+    let textBox = await driver.findElement(By.name('my-text'));
+    let submitButton = await driver.findElement(By.css('button'));
+  
+    await textBox.sendKeys('Selenium');
+    await submitButton.click();
+  
+    let message = await driver.findElement(By.id('message'));
+    let value = await message.getText();
+    assert.equal("Received!", value);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await driver.quit();
+  }
+}
+
+firstTest();
