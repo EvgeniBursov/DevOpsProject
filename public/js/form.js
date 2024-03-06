5946727093a001a5dda27d0388b74847911cebd1
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-03-06 13:12:38 +0200

    change pages

--- public/js/form.js
+++ public/js/form.js
@@ -1,28 +1,24 @@
+//import User from "../../models/user.js"
 const loader = document.querySelector('.loader');
-
 // select inputs 
 const submitBtn = document.querySelector('.submit-btn');
 const nameUser = document.querySelector('#name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const number = document.querySelector('#number');
-const tac = document.querySelector('#terms-and-cond');
-const notification = document.querySelector('#notification');
 
-
+ 
 submitBtn.addEventListener('click', () => {
-    if(nameUser.value.length < 3){
+    if(nameUser.value.length <1){
         showAlert('name must be 3 letters long');
     } else if(!email.value.length){
         showAlert('enter your email');
-    } else if(password.value.length < 8){
+    } else if(password.value.length < 1){
         showAlert('password should be 8 letters long');
     } else if(!number.value.length){
         showAlert('enter your phone number');
-    } else if(!Number(number.value) || number.value.length < 10){
+    } else if(!Number(number.value) || number.value.length < 1){
         showAlert('invalid number, please enter valid one');
-    } else if(!tac.checked){
-        showAlert('you must agree to our terms and conditions');
     } else{
         loader.style.display = 'block'
         sendData('/signup', {
@@ -30,13 +26,20 @@ submitBtn.addEventListener('click', () => {
             email: email.value,
             password: password.value,
             number: number.value,
-            tac: tac.checked,
-            notification: notification.checked,
-            seller: false
         })
     }
 })
 
+
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 1500);
+}
 
 const sendData = (path, data) => {
     fetch(path, {
@@ -49,13 +52,13 @@ const sendData = (path, data) => {
     })
 }
 
-const showAlert = (msg) => {
-    let alertBox = document.querySelector('.alert-box');
-    let alertMsg = document.querySelector('.alert-msg');
-    alertMsg.innerHTML = msg;
-    alertBox.classList.add('show');
-    setTimeout(() => {
-        alertBox.classList.remove('show');
-    }, 1500);
+const processData = (data) => {
+    loader.style.display = null;
+    if(data.alert){
+        showAlert(data.alert);
+    } else if(data.name){
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    }
 }
 
