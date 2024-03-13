fa77016e24841212430bc1251b2b3208b8a3cc4f
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-03-13 14:26:21 +0200

    add webdriver selenium tests

--- tests/first.js
+++ tests/first.js
@@ -1,4 +1,4 @@
-import { By, Builder } from 'selenium-webdriver';
+import { By, Builder, Key, until } from 'selenium-webdriver';
 import assert from 'assert';
 
 async function firstTest() {
@@ -11,6 +11,7 @@ async function firstTest() {
     let title = await driver.getTitle();
     assert.equal("Web Store", title);
 
+    /*
     await driver.navigate().to('https://devopsproject-v74y.onrender.com/signup')
     title = await driver.getTitle();
     assert.equal("Sign Up", title);
@@ -21,11 +22,63 @@ async function firstTest() {
     assert.equal("Web Store", title);
     await driver.sleep(1500)
 
-    await driver.navigate().forward()
-    await driver.sleep(1500)
-    await driver.navigate().to('https://devopsproject-v74y.onrender.com/404')
+    await driver.navigate().to('https://devopsproject-v74y.onrender.com/login')
+    title = await driver.getTitle();
+    assert.equal("Login Page", title);
+    await driver.sleep(1500)*/
+
+    /*login check*/ 
+    /*await driver.wait(until.elementLocated(By.id('email')), 5000);
+    await driver.findElement(By.id('email')).sendKeys('Bursov@gmail.com');
+    await driver.sleep(3500)
+    await driver.findElement(By.id('password')).sendKeys('123456');
+    await driver.sleep(3500)
+    await driver.findElement(By.className('submit-btn')).click();
+    await driver.sleep(3500)
+    await driver.wait(until.urlContains('https://devopsproject-v74y.onrender.com/'), 5000);
+    let dashboardPageUrl = await driver.getCurrentUrl();
+    if (dashboardPageUrl.includes('https://devopsproject-v74y.onrender.com/')) {
+      console.log("User exists. Logged in successfully.");
+  } else {
+      console.log("User doesn't exist or login failed.");
+  }*/
+
+  /*register check*/ 
+  /*
+  await driver.findElement(By.id('user-img')).click();
+  await driver.sleep(3500)
+  await driver.findElement(By.className('reg_btn')).click();
+  await driver.sleep(2500)
+
+  await driver.wait(until.elementLocated(By.id('name')), 5000);
+  await driver.findElement(By.id('name')).sendKeys('Rita');
+
+  await driver.wait(until.elementLocated(By.id('email')), 5000);
+  await driver.findElement(By.id('email')).sendKeys('rita1234@gmail.com');
+
+  await driver.wait(until.elementLocated(By.id('password')), 5000);
+  await driver.findElement(By.id('password')).sendKeys('123456');
+
+  await driver.wait(until.elementLocated(By.id('number')), 5000);
+  await driver.findElement(By.id('number')).sendKeys('987654321');
+
+  await driver.findElement(By.className('submit-btn')).click();
+  await driver.sleep(3500)
+
+  await driver.wait(until.urlContains('https://devopsproject-v74y.onrender.com/'), 5000);
+  let pageUrl = await driver.getCurrentUrl();
+  if (pageUrl.includes('https://devopsproject-v74y.onrender.com/')) {
+    console.log("User register successfully.");
+} else {
+    console.log("User registration failed.");
+}*/
+
+
+  //await driver.navigate().forward()
+  //await driver.sleep(1500)
+  //await driver.navigate().to('https://devopsproject-v74y.onrender.com/404')
   
-    await driver.manage().setTimeouts({ implicit: 10000 });
+    await driver.manage().setTimeouts({ implicit: 50000 });
   
     /*let textBox = await driver.findElement(By.name('my-text'));
     let submitButton = await driver.findElement(By.css('button'));
