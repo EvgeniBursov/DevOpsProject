32118c140efe8f94b6acfa4354109b61aa442394
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-04-17 12:03:24 +0300

    check test

--- tests/selenium/404Test.spec.js
+++ tests/selenium/404Test.spec.js
@@ -1,4 +1,4 @@
-import { By, Builder, until } from 'selenium-webdriver';
+import { By, Builder, Key, until} from 'selenium-webdriver';
 import assert from 'assert';
 import { describe, it, beforeEach, afterEach } from 'mocha';
 
@@ -11,21 +11,29 @@ describe('Page not found', () =>{
     driver = await new Builder().forBrowser('chrome').build()
     await driver.manage().setTimeouts({ implicit: 30000 });
   })
+
   afterEach(async function () {
     await driver.manage().setTimeouts({ implicit: 30000 });
     await driver.quit();
   })
-  it('404 page', async function () {
+
+  it('Web Store page', async function () {
+    this.timeout(10000); // Adjust timeout if needed
     await driver.get('https://devopsproject-v74y.onrender.com/');
     let title = await driver.getTitle();
     assert.equal("Web Store", title);
-    await driver.sleep(2500)
+    await driver.sleep(5500)
+  })
+
+  it('Page Not Found', async function () {
+    this.timeout(10000); // Adjust timeout if needed
     await driver.get('https://devopsproject-v74y.onrender.com/lsasdls');
-    title = await driver.getTitle();
+    let title = await driver.getTitle();
     assert.equal("Page Not Found", title);
-    await driver.sleep(3500)
+    await driver.sleep(5500)
     await driver.wait(until.urlContains('https://devopsproject-v74y.onrender.com/404'), 5000);
   })
+
 })
 /*
 async function notFoungPageTest() {
