bfc1d03103907c221596bc9b46e92a8ea5b8e193
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-04-18 14:29:58 +0300

    check tests ide

--- tests/seleniumIDE/register.spec.js
+++ tests/seleniumIDE/register.spec.js
@@ -3,8 +3,7 @@ import assert from 'assert';
 import { describe, beforeEach, afterEach, it } from 'mocha';
 
 
-
-describe('register', function() {
+describe('Register', function() {
   this.timeout(50000)
   let driver
   let vars
@@ -15,43 +14,34 @@ describe('register', function() {
   afterEach(async function() {
     await driver.quit();
   })
-  it('register', async function() {
+  it('Register', async function() {
     await driver.get("https://devopsproject-v74y.onrender.com/")
     await driver.manage().window().setRect({ width: 1552, height: 840 })
     await driver.findElement(By.id("user-img")).click()
     await driver.findElement(By.id("user-btn")).click()
-    await driver.findElement(By.id("name")).click()
     await driver.findElement(By.css(".submit-btn")).click()
+    await driver.findElement(By.css("body")).click()
     await driver.findElement(By.id("name")).click()
-    await driver.findElement(By.id("name")).sendKeys("Evgeni")
+    await driver.findElement(By.id("name")).sendKeys("asd")
     await driver.findElement(By.css(".submit-btn")).click()
     await driver.findElement(By.id("email")).click()
     await driver.findElement(By.id("email")).sendKeys("Bursov@gmail.com")
     await driver.findElement(By.css(".submit-btn")).click()
-    await driver.findElement(By.css(".container > div")).click()
     await driver.findElement(By.id("password")).click()
+    await driver.findElement(By.css("body")).click()
+    await driver.findElement(By.css("body")).click()
     await driver.findElement(By.id("password")).sendKeys("123456")
-    await driver.findElement(By.css(".submit-btn")).click()
     await driver.findElement(By.id("number")).click()
-    await driver.findElement(By.id("number")).sendKeys("1234567890")
+    await driver.findElement(By.id("number")).sendKeys("123")
     await driver.findElement(By.css(".submit-btn")).click()
+    await driver.findElement(By.css("body")).click()
     await driver.findElement(By.id("email")).click()
-    await driver.findElement(By.id("email")).click()
-    await driver.findElement(By.id("email")).click()
-    await driver.findElement(By.id("email")).click()
-    {
-      const element = await driver.findElement(By.id("email"))
-      await driver.actions({ bridge: true}).doubleClick(element).perform()
-    }
-    await driver.findElement(By.id("email")).sendKeys("Evgeni3@gmail.com")
+    await driver.findElement(By.id("email")).sendKeys("Bursov12345@gmail.com")
     await driver.findElement(By.css(".submit-btn")).click()
     await driver.findElement(By.id("user-img")).click()
     await driver.findElement(By.css(".login-logout-popup")).click()
-    await driver.findElement(By.id("user-btn")).click()
-    await driver.findElement(By.css(".nav-items")).click()
-    await driver.findElement(By.css(".nav-items")).click()
-    await driver.findElement(By.id("user-img")).click()
     await driver.findElement(By.css(".hero-section")).click()
+    await driver.findElement(By.id("user-btn")).click()
     await driver.close()
   })
 })
