77c16f4d26bc6f2965091fcde77c41128ef136b8
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-05-13 13:43:25 +0300

    test on firefox

--- tests/seleniumGrid/test.js
+++ tests/seleniumGrid/test.js
@@ -25,6 +25,14 @@ async function loginTest(browser) {
       .usingWebDriverProxy('http://selenium-hub:4444')
       .withCapabilities(firefoxCapabilities)
       .build();
+      try {
+        await driver.get('https://www.google.com');
+        //        await driver.get('http://localhost:5000');
+        const title = await driver.getTitle();
+        console.log('Page title:', title);
+      } finally {
+        await driver.quit();
+      }
   } else {
     console.log("Unsupported browser.");
     return;
@@ -35,8 +43,8 @@ async function loginTest(browser) {
 
 async function runTests() {
   // Run tests in Chrome
-  console.log("Running tests in Chrome...");
-  await loginTest('chrome');
+  //console.log("Running tests in Chrome...");
+ // await loginTest('chrome');
   // Run tests in Firefox
   console.log("Running tests in Firefox...");
   await loginTest('firefox');
