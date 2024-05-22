860ba63f2ae9fa741e5b4e11996c50ef4fca5a49
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-05-22 11:39:16 +0300

    add grid testing

--- tests/seleniumGrid/test.js
+++ tests/seleniumGrid/test.js
@@ -7,17 +7,14 @@ async function loginTest(browser) {
   let driver;
 
   if (browser === 'chrome') {
-    console.log("before tests build")
     driver = await new Builder()
       .usingServer('http://localhost:4444')
       .forBrowser('chrome')
       .build();
-      console.log("after build")
       try {
-        console.log("before connecting localhost 5000")
         await driver.get('https://devopsproject-v74y.onrender.com');
         const title = await driver.getTitle();
-        console.log('Page title:', title);
+        console.log('Page title from chrome:', title);
       }finally {
         await driver.quit();
     }
@@ -25,59 +22,66 @@ async function loginTest(browser) {
     const firefoxCapabilities = Capabilities.firefox();
     driver = await new Builder()
       .forBrowser('firefox')
-      .usingServer('http://localhost:4444/wd/hub')
+      .usingServer('http://localhost:4444')
       .withCapabilities(firefoxCapabilities)
       .build();
-  } else {
+      try {
+        await driver.get('https://devopsproject-v74y.onrender.com');
+        const title = await driver.getTitle();
+        console.log('Page title from firefox:', title);
+      }finally {
+        await driver.quit();
+    }
+  } else if (browser === 'edge') {
+    const edgeCapabilities = Capabilities.edge();
+    driver = await new Builder()
+      .forBrowser('edge')
+      .usingServer('http://localhost:4444')
+      .withCapabilities(edgeCapabilities)
+      .build();
+      try {
+        await driver.get('https://devopsproject-v74y.onrender.com');
+        const title = await driver.getTitle();
+        console.log('Page title from edge:', title);
+      }finally {
+        await driver.quit();
+    }
+  }else if (browser === 'safari') {
+    const safariCapabilities = Capabilities.safari();
+    driver = await new Builder()
+      .forBrowser('safari')
+      .usingServer('http://localhost:4444')
+      .withCapabilities(safariCapabilities)
+      .build();
+      try {
+        await driver.get('https://devopsproject-v74y.onrender.com');
+        const title = await driver.getTitle();
+        console.log('Page title from safari:', title);
+      }finally {
+        await driver.quit();
+    }
+  }else {
     console.log("Unsupported browser.");
     return;
   }
 }
 
-await loginTest('chrome')
 
-  /*try {
-    await driver.get('https://devopsproject-v74y.onrender.com');
-    const title = await driver.getTitle();
-    console.log('Page title:', title);
-  } finally {
-    await driver.quit();
-  }*/
-///}
-
-/*async function runTests() {
-  // Run tests in Chrome
-  //console.log("Running tests in Chrome...");
-  //await loginTest('chrome');
+async function runTests() {
+  console.log("Running tests in Chrome...");
+  await loginTest('chrome');
   
-  // Run tests in Firefox
   console.log("Running tests in Firefox...");
   await loginTest('firefox');
 
+  console.log("Running tests in Safari...");
+  await loginTest('safari');
+
+  console.log("Running tests in Edge...");
+  await loginTest('edge');
   console.log("All tests completed.");
 }
 
-runTests();*/
+runTests();
 
-
-/*import { By, Builder, Key, until} from 'selenium-webdriver';
-import { Capabilities } from 'selenium-webdriver';
-
-
-async function exampleTest() {
-    const driver = await new Builder()
-        .forBrowser('chrome')
-        .usingServer('http://selenium-hub:4444/wd/hub')
-        .build();
-
-    try {
-        await driver.get('https://www.example.com');
-        const title = await driver.getTitle();
-        console.log('Page title:', title);
-    } finally {
-        await driver.quit();
-    }
-}
-
-exampleTest();*/
 
